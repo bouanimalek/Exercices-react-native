64d910a147457eb12406f8a6145aaee8137aa235
malzak <malzovich@gmail.com>
2022-02-09 12:08:10 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const obj = [
+    { id: 1, name: "Stan", age: 45 },
+    { id: 2, name: "Francine", age: 45 },
+    { id: 3, name: "Hayley", age: 18 },
+    { id: 4, name: "Steve", age: 14 },
+    { id: 5, name: "Roger", age: 1020 },
+    { id: 6, name: "Klaus", age: 35 },
+  ];
+
+  return <View style={styles.container}></View>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
